95a9608c6bf52402a213e356c6e51f55f10eb815
jruh <>
2020-11-19 13:26:20 -0500

    script prospector
    scripts prospector

---
+++ Assets/Scripts/ProspectorStethoscope.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProspectorStethoscope : MonoBehaviour
+{
+	public Vector3 mousePosition;
+	
+	void Start()
+	{
+	
+	}
+	
+	
+	void Update()
+	{
+		mousePosition = Input.mousePosition;
+	}
+}
